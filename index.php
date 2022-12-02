b7a1401b6a49f0ed547b39d8dacc4e707a92c297
“Ibra1822” <ibrahimmujagic18@gmail.com>
2022-12-02 17:02:41 +0100

    add milestone 1

--- index.php
+++ index.php
@@ -1,14 +1,73 @@
+<?php 
+
+$letters='abcdefghilmnopqrstuezABCDEFGHILMNOPQRSTUEZ';
+$numbers = '0123456789';
+$special = '!?&%$<>^+-*/()[]{}@#_=';
+
+$finalString ='';
+$finalString.= $letters;
+$finalString.=$numbers;
+$finalString.=$special;
+
+
+$all = '';
+
+$repeat = $_GET['tot'];
+
+
+if (!empty($repeat)) {
+  while(strlen($all) < $repeat ){
+    $all .= $finalString[rand(0,(strlen($finalString) -1 ))];
+  }
+}
+
+
+var_dump($all);
+
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport" content="width=device-width  initial-scale=1.0">
   <title>Password Generator</title>
 </head>
+<style>
+  h1, h2,h3,form{
+    text-align: center;
+  }
+  input,button{
+    padding: 10px 15px;
+  }
+  .ris{
+    text-align: center;
+  }
+
+</style>
 <body>
 
-hello
+  <div class="cont-title" >
+    <h1>Crea una password sicura</h1>
+    <h2> Inserisci quanti caratteri vuoi nella tua password</h2>
+  </div> 
   
+
+  <form action="./index.php" method="GET">
+
+    <input type="number" name="tot">
+
+    <button type="submit" >Crea</button>
+
+  </form>
+
+  <div class="ris"  >
+    <h3> La tua password è: <?php echo $all ?></h3> 
+  </div>
+
 </body>
 </html>
