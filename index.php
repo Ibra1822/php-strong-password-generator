ed7dd7fca9928c30774e29f1c18f33de9adabf70
“Ibra1822” <ibrahimmujagic18@gmail.com>
2022-12-02 17:14:42 +0100

    add milestone 2

--- index.php
+++ index.php
@@ -1,29 +1,6 @@
 <?php 
 
-$letters='abcdefghilmnopqrstuezABCDEFGHILMNOPQRSTUEZ';
-$numbers = '0123456789';
-$special = '!?&%$<>^+-*/()[]{}@#_=';
-
-$finalString ='';
-$finalString.= $letters;
-$finalString.=$numbers;
-$finalString.=$special;
-
-
-$all = '';
-
-$repeat = $_GET['tot'];
-
-
-if (!empty($repeat)) {
-  while(strlen($all) < $repeat ){
-    $all .= $finalString[rand(0,(strlen($finalString) -1 ))];
-  }
-}
-
-
-var_dump($all);
-
+require './functions.php'
 
 ?>
 
