96665fcdca01025bee6d89fb1e9bfb5d205694bc
“Ibra1822” <ibrahimmujagic18@gmail.com>
2022-12-04 15:15:35 +0100

    fixed all

--- functions.php
+++ functions.php
@@ -10,20 +10,36 @@ $finalString.=$special;
 
 $all = '';
 
-$repeat = $_GET['tot'];
-
+$repeat = $_GET['tot'] ?? '';
+$isIn= false;
+$error = false;
+$output = '';
 if (!empty($repeat)) {
-  $isIn = true;
-  while(strlen($all) < $repeat ){
+
+  if ($repeat < 8 || $repeat > 32 ) {
+    $isIn= false;
+    $error = true;
+    $output = 'Il numero minimo di caratteri della password è 8 e massimo 32';
+  }else{
+     $isIn = true;
+    while(strlen($all) < $repeat ){
     $all .= $finalString[rand(0,(strlen($finalString) -1 ))];
   }
+  }
+
   if($isIn){
     session_start();
     header('Location: ./result.php');
     $_SESSION['all'] = $all;
   }
- 
-}
+} 
+
+$check = $_GET['characters'] ?? '';
+
+var_dump($check);
+
+
+
 
 
 
--- index.php
+++ index.php
@@ -25,14 +25,27 @@ require './functions.php'
     <h1>Crea una password sicura</h1>
     <h2> Inserisci quanti caratteri vuoi nella tua password</h2>
   </div> 
-  
+  <?php if($error === true): ?>
+  <div class="error" >
+    <h2><?php echo $output?></h2>
+  </div>
+  <?php endif; ?>
 
   <form action="./index.php" method="GET">
 
     <input type="number" name="tot">
 
-    <button type="submit" >Crea</button>
+    <div class="cont-check">
+    <input type="checkbox" name="characters[]" value="0" id="">
+    <label for="">Lettere</label>
+    <input type="checkbox" name="characters[]" value="1" id="">
+    <label for="">Numeri</label>
+    <input type="checkbox" name="characters[]" value="2" id="">
+    <label for="">Simboli</label>
+    </div>
 
+
+    <button type="submit" >Crea</button>
   </form>
 
 
